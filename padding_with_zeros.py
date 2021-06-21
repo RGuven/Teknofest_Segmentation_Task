a3d2966bdbbf3c68014c3637f551502f03afd095
RGuven <rguven887@gmail.com>
2021-06-21 23:26:47 +0300

    padding with zeros working now

--- padding_with_zeros.py
+++ padding_with_zeros.py
@@ -11,49 +11,73 @@ from PIL import Image
 import numpy as np
 import matplotlib.pyplot as plt
 
-IMAGE_PATH = "./roi_images/"
+
+
 target_size = 512  # 512x512
 
-DEST_PATH = "./roi_images_padding_zeros/"
 
-i=0
-for image in os.listdir(IMAGE_PATH):
-    
-   
-    file_name   = image.split(".")[0]
-    img = Image.open(IMAGE_PATH + file_name + '.jpg')
-    width, height = img.size
-    
-    if width < 512  or height < 512:
-        
-        left_side = int((target_size - width) / 2)
-        top_side = int((target_size - height) / 2)
-        result = Image.new(img.mode, (target_size, target_size), (0,0,0))
-        result.paste(img, (left_side, top_side))
-        """
-        saving way not tested
-        result.save("{}{}.jpg".format(DEST_PATH,file_name))
-        """
-        #print('Image Size', np.asarray(img).shape)
-        #print('Image Size', np.asarray(result).shape)
-        
-        #plt.imshow(img)
-        #plt.show()
-        #plt.imshow(result)
-        #plt.show()
-        
+def padding_zeros(IMAGE_PATH,DEST_IMAGE_PATH,is_mask):
+    i=0
+    if is_mask:
+        file_extention = ".png"
     else:
-        
-        print(file_name)
-        img = img.resize((512,512))
+        file_extention = ".jpg"
 
-            
-            
+    
+    for image in os.listdir(IMAGE_PATH):
         
+        file_name   = image.split(".")[0]
+        img = Image.open(IMAGE_PATH + file_name + file_extention)
+        width, height = img.size
         
+        if width < 512  or height < 512:
+            
+            left_side = int((target_size - width) / 2)
+            top_side = int((target_size - height) / 2)
+            print(img.mode)
+            
+            if is_mask:
+                #THIS FOR PNG IMAGE IN PILLOW :)
+                
+                result = Image.new(img.mode, (target_size, target_size), (0))
+            else:
+                result = Image.new(img.mode, (target_size, target_size), (0,0,0))
+                
+            result.paste(img, (left_side, top_side))
+    
+            result.save("{}{}{}".format(DEST_IMAGE_PATH,file_name,file_extention))
+     
+            #print('Image Size', np.asarray(img).shape)
+            #print('Image Size', np.asarray(result).shape)
+            
+            #plt.imshow(img)
+            #plt.show()
+            #plt.imshow(result)
+            #plt.show()
+        else:
+            
+            print(file_name)
+            img = img.resize((512,512))
+            img.save("{}{}{}".format(DEST_IMAGE_PATH,file_name,file_extention))
+    
+                
+                
+            
+#this for images padding
+
+padding_zeros(IMAGE_PATH = "./roi_images/",
+              DEST_IMAGE_PATH = "./roi_images_padding_zeros/",
+              is_mask=False
+             )       
     
     
     
-    
+#this for maskes
+
+padding_zeros(IMAGE_PATH = "./roi_maskes/",
+              DEST_IMAGE_PATH = "./roi_maskes_padding_zeros/",
+              is_mask=True
+             )       
+     
     
     
